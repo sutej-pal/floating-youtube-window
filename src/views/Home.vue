f7987471e3c6731aadbc262d8ff098ecddec78c8
sutej-truetechpro <sutej@truetechpro.com>
2020-04-24 16:28:09 +0530

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-main">
+    <div class="drag-div" style="-webkit-app-region: drag"></div>
+    <div class="splash" v-if="iframeUrl === ''">
+      <div class="d-flex flex-column w-75 pt-5 mx-auto" ref="input-form">
+        <h3 class="text-secondary">Paste YouTube url</h3>
+        <input type="text" class="form-control" v-model="newUrl" @keyup.enter="changeVideo('splash')">
+<!--        <button class="btn w-25 mx-auto mt-3" style="min-width: 130px" @click="changeVideo('splash')">Open</button>-->
+      </div>
+    </div>
+    <iframe v-else width="100%" height="100%" frameborder="0"
+            :src="iframeUrl">
+    </iframe>
+    <div class="new-url">
+      <button class="btn float-right" @click="$refs['input-form'].classList.toggle('d-none')">New Url</button>
+    </div>
+    <div class="new-url-form flex-column w-75 d-none p-2" ref="input-form">
+      <input type="text" class="form-control" v-model="newUrl" @keyup.enter="changeVideo()">
+<!--      <button class="btn w-25 mx-auto mt-3" @click="changeVideo()">Open</button>-->
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script src="./home.js"/>
+<style lang="scss" scoped>
+  .home-main {
+    height: 100%;
+    overflow: hidden;
+    position: relative;
+    .drag-div {
+      position: absolute;
+      right: 0;
+      left: 0;
+      height: 10%;
+      z-index: 10;
+    }
+    .splash {
+      height: 100%;
+      width: 100%;
+      background: rgba(44, 62, 80, 0.38);
+    }
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+    .new-url {
+      position: absolute;
+      top: 50%;
+      right: 0;
+      height: 30px;
+      width: 130px;
+
+      &:hover {
+
+        button {
+          display: block;
+        }
+      }
+
+      button {
+        display: none;
+      }
+    }
+    .new-url-form {
+      position: absolute;
+      top: 30%;
+      left: 0;
+      right: 0;
+      margin: auto;
+      display: flex;
+      background: rgba(44, 62, 80, 0.33);
+      button {
+        min-width: 130px;
+      }
+    }
   }
-}
-</script>
+</style>
---
+++ src/views/home.js
@@ -0,0 +1,27 @@
+export default {
+  data() {
+    return {
+      youTubeUrl: '',
+      newUrl: '',
+      iframeUrl: ''
+    }
+  },
+  methods: {
+    changeVideo(from) {
+      if (this.newUrl === '') {
+        return
+      }
+      let videoId;
+      const temp = this.newUrl.split('?v=');
+      if (temp[1].includes('&')) {
+        videoId = temp[1].split('&')[0];
+      } else {
+        videoId = temp[1];
+      }
+      this.iframeUrl = 'https://www.youtube.com/embed/' + videoId;
+      if (from !== 'splash') {
+        this.$refs['input-form'].classList.toggle('d-none')
+      }
+    }
+  }
+}
